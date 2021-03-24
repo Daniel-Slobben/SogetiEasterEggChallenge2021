c4e669691dff0e2e3c5b392fc43c234308f94c79
Slobben <daniel.slobben@sogeti.com>
2021-03-24 08:41:37 +0100

    Moved the beginning up a pixel, and some minor changes

--- src/nl/sogeti/com/drawing/CellularGenerator.java
+++ src/nl/sogeti/com/drawing/CellularGenerator.java
@@ -17,18 +17,18 @@ public class CellularGenerator {
       colorMatrix = new Colors[eggMetrics.getFrameHeight()+1][eggMetrics.getFrameWidth()+1];
 
       // Set our initial pixel to true
-      matrix[1][eggMetrics.getFrameWidth()/2] = true;
+      matrix[0][eggMetrics.getFrameWidth()/2] = true;
    }
 
    public Colors[][] generate() {
       System.out.println(eggMetrics.getFrameWidth());
-      for (int i = 1; i < eggMetrics.getFrameHeight(); i++) {
+      for (int i = 0; i < eggMetrics.getFrameHeight(); i++) {
          for (int j = 1; j < eggMetrics.getFrameWidth(); j++) {
 
             // matrix i+1 j is the target we will decide for
             boolean[] cellValues =  {matrix[i][j - 1], matrix[i][j], matrix[i][j+1]};
 
-            int cellValueInteger = decide(cellValues);
+            int cellValueInteger = booleanArrayToInteger(cellValues);
             matrix[i+1][j] = ruleSet[cellValueInteger];
             if (ruleSet[cellValueInteger]) {
                colorMatrix[i+1][j] = getColor(cellValueInteger);
@@ -44,7 +44,7 @@ public class CellularGenerator {
     *
     * @return
     */
-   private int decide(boolean[] cellValues) {
+   private int booleanArrayToInteger(boolean[] cellValues) {
       StringBuilder binaryCellValues = new StringBuilder();
       for (boolean cellValue: cellValues) {
          if (cellValue) {
@@ -62,9 +62,9 @@ public class CellularGenerator {
     */
    private Colors getColor(int decimal) {
       return switch (decimal) {
-         case 1 -> Colors.GREEN;
-         case 2 -> Colors.RED;
-         case 3 -> Colors.PURPLE;
+         case 1 -> Colors.YELLOW;
+         case 2 -> Colors.PURPLE;
+         case 3 -> Colors.CYAN;
          case 4 -> Colors.BLUE;
          default -> Colors.WHITE;
       };
